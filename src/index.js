e1b71e0278619b708f73440244f7eedba529ac7c
Travis Payne <thpaggie910@gmail.com>
2017-09-13 11:38:49 -0500

    Install jQuery and modify the DOM with it

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
+const $ = require("jquery");
+
 const hello = () => console.log("hello world from webpack!");
 
 hello();
+
+$("body")
+    .html("<h1>Hello From jQuery</h1>")
+    .css("background-color", "gray");
